987dfad36c9e0b71e9a0f6d4e3d872620fb5f163
tom-overton <tom.overton@outlook.com>
2018-02-07 00:02:58 -0800

    Find the class data block by actually following pointers instead of just hardcoding the offset

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # fates-enemy-growth-modifier
 ![Example screenshot](http://i.imgur.com/FwIsQcf.png)
 
-This tool is really quick and dirty; I hardcoded the offsets for the class entries, so it probably doesn't work for modified GameData.bin files.
+This tool is really quick and dirty, so it probably doesn't work for modified GameData.bin files.
 
 To use this tool:
 1. Use [FEAT](https://github.com/SciresM/FEAT) to decompress your GameData.bin.lz file.
@@ -15,4 +15,4 @@ If you want to experiment with this tool without extracting the GameData.bin fro
 
 I also included the ability to increase every growth for every class by a given amount (this was the whole reason I built this in the first place). Use this to make the game universally harder or easier.
 
-To build the source code, just compile the solution within Visual Studio. I used VS Community 2015, but 2017 probably works as well.
+To build the source code, just compile the solution within Visual Studio. I used VS Community 2015 and 2017 while developing this.
--- src/GameDataModel.cs
+++ src/GameDataModel.cs
@@ -1,20 +1,38 @@
 ﻿// Copyright (c) 2017 Tom Overton
 
+using System;
 using System.IO;
 
 namespace FatesEnemyGrowthModifier
 {
     public class GameDataModel
     {
-        private const int START_OF_CLASS_ENTRIES = 0xEA10;
+        // Constants obtained from here: https://github.com/RainThunder/fefates-tools/wiki/BIN-(File-Format)
+        private const int GAMEDATA_HEADER_SIZE = 0x20;
+        private const int POINTER_SIZE = 0x4;
+        private const int DATA_REGION_SIZE_OFFSET = 0x4;
+
+        // I *think* class data is always the third block in the data region?
+        // That's what this document implies: https://pastebin.com/3zHxeqUj
+        private const int CLASS_DATA_BLOCK_INDEX = 2;
+
+        // It appears the class block itself has a small header on it?
+        // On a vanilla GameData.bin, the pointer points us to 0xEA08,
+        // but the actual class data starts at 0xEA10
+        private const int CLASS_DATA_HEADER_SIZE = 0x8;
+
         private FileStream file;
+        private int dataRegionSize;
+        private int startOfClassData;
 
         public ClassEntry[] classEntries = new ClassEntry[Constants.NUMBER_OF_CLASSES];
 
         public GameDataModel(string filename)
         {
             this.file = File.Open(filename, FileMode.Open, FileAccess.ReadWrite);
-            this.file.Seek(START_OF_CLASS_ENTRIES, SeekOrigin.Begin);
+            this.dataRegionSize = this.ReadInt32FromFileAtOffset(DATA_REGION_SIZE_OFFSET);
+            this.LocateStartOfClassData();
+            this.file.Seek(this.startOfClassData + CLASS_DATA_HEADER_SIZE, SeekOrigin.Begin);
             byte[] classEntryBytes = new byte[Constants.CLASS_ENTRY_SIZE];
             for (int i = 0; i < Constants.NUMBER_OF_CLASSES; i++)
             {
@@ -23,10 +41,18 @@ namespace FatesEnemyGrowthModifier
             }
         }
 
+        public void LocateStartOfClassData()
+        {
+            int startOfPointerRegion1 = GAMEDATA_HEADER_SIZE + dataRegionSize;
+            int offset1 = startOfPointerRegion1 + (POINTER_SIZE * CLASS_DATA_BLOCK_INDEX);
+            int pointerToClassDataPointer = this.ReadInt32FromFileAtOffset(offset1) + GAMEDATA_HEADER_SIZE;
+            this.startOfClassData = this.ReadInt32FromFileAtOffset(pointerToClassDataPointer) + GAMEDATA_HEADER_SIZE;
+        }
+
         public void WriteCurrentModelToFile()
         {
             // Seek to the first enemy growth entry
-            this.file.Seek(START_OF_CLASS_ENTRIES, SeekOrigin.Begin);
+            this.file.Seek(this.startOfClassData + CLASS_DATA_HEADER_SIZE, SeekOrigin.Begin);
             this.file.Seek(ClassOffsets.ENEMY_HP_GROWTH, SeekOrigin.Current);
 
             for (int i = 0; i < this.classEntries.Length; i++)
@@ -58,6 +84,28 @@ namespace FatesEnemyGrowthModifier
                 this.classEntries[i].EnemyDefenseGrowth = AddValueToByteWithoutOverflow(this.classEntries[i].EnemyDefenseGrowth, value);
                 this.classEntries[i].EnemyResistanceGrowth = AddValueToByteWithoutOverflow(this.classEntries[i].EnemyResistanceGrowth, value);
             }
+        }
+
+        private int ReadInt32FromFileAtOffset(int offset)
+        {
+            this.file.Seek(offset, SeekOrigin.Begin);
+            byte[] dataBytes = new byte[4];
+            this.file.Read(dataBytes, 0, 4);
+
+            // Fire Emblem Fates stores data in little-endian format.
+            // If the current platform is big endian, reverse the bytes
+            // so that BitConverter reads them in correctly.
+            if (!BitConverter.IsLittleEndian)
+            {
+                byte temp = dataBytes[0];
+                dataBytes[0] = dataBytes[3];
+                dataBytes[3] = temp;
+                temp = dataBytes[1];
+                dataBytes[1] = dataBytes[2];
+                dataBytes[2] = temp;
+            }
+
+            return BitConverter.ToInt32(dataBytes, 0);
         }
 
         private byte AddValueToByteWithoutOverflow(byte b, int value)
